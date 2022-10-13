338fb29666f03a1c5a608ca108da1f38431b495f
anniep8911 <pjatopazz@gmail.com>
2022-10-13 23:27:09 +0900

    ver02

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
-  publicPath: '/mytripVue',
+  publicPath: '/mytrip',
   outputDir: 'docs' // build 저장 파일 위치
 });
 chainWebpack: (config) => {
